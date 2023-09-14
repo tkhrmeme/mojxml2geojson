28da4131b8266285648f153282804fd926eb13d8
Takahiro Endo <takahiro.consadole.endo@gmail.com>
2023-09-14 13:26:25 +0900

    出力先ディレクトリをオプション指定する

--- src/mojxml2geojson/core.py
+++ src/mojxml2geojson/core.py
@@ -10,6 +10,7 @@ def get_args():
 
     parser.add_argument('FILE_PATH')
     parser.add_argument('-e', '--exclude', help='地番が地図外、別図の筆を除外して出力', action='store_true')
+    parser.add_argument('-o', '--output', help='ファイル出力先のディレクトリパス', action='store')
     args = parser.parse_args()
 
     return args
@@ -18,10 +19,11 @@ def get_args():
 def main():
     args = get_args()
     srcFile = args.FILE_PATH
+    dstDir = args.output
     exclude_flag = args.exclude
 
     try:
-        package.xml2geojson.SaveGeoJson(srcFile, exclude_flag)
+        package.xml2geojson.SaveGeoJson(srcFile, dstDir, exclude_flag)
     except Exception:
         print('Error Source File:', srcFile)
         traceback.print_exc()
--- src/package/xml2geojson.py
+++ src/package/xml2geojson.py
@@ -8,12 +8,21 @@ import package.MojXmlPolygon as MojXmlPolygon
 import package.MojXMLtoGeoJSON as MojXMLtoGeoJSON
 
 
-def SaveGeoJson(src_file, exclude_flag):
+def SaveGeoJson(src_file, dst_directory, exclude_flag):
     moj_obj = MojXml(src_file)
 
     mojGeojson = MojGeojson(moj_obj, exclude_flag)
 
-    dst_name = pathlib.Path(src_file).with_suffix(".geojson").resolve()
+    if dst_directory is not None:
+        path_dst = pathlib.Path(dst_directory).resolve()
+        path_dst.mkdir(exist_ok = True)
+
+        if path_dst.is_dir():
+            path_src = pathlib.Path(src_file).resolve()
+            dst_name = (path_dst / path_src.name).with_suffix(".geojson")
+    else:
+        # 出力先が指定されていない場合はXMLファイルと同じディレクトリに作成
+        dst_name = pathlib.Path(src_file).with_suffix(".geojson").resolve()
 
     with open(dst_name, 'w', encoding='utf-8') as f:
         f.write(mojGeojson)
