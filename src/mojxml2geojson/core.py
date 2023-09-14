2c26f8db7d48c65ef2bca0418225f74ee1082fcd
Takahiro Endo <takahiro.consadole.endo@gmail.com>
2023-09-14 20:34:45 +0900

    .zipを展開してGeoJSONに変換 #2

--- src/mojxml2geojson/core.py
+++ src/mojxml2geojson/core.py
@@ -3,7 +3,9 @@
 import argparse
 import traceback
 import package.xml2geojson
-
+import pathlib
+import zipfile
+import tempfile
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -23,7 +25,35 @@ def main():
     exclude_flag = args.exclude
 
     try:
-        package.xml2geojson.SaveGeoJson(srcFile, dstDir, exclude_flag)
+        path_src = pathlib.Path(srcFile)
+
+        if not path_src.exists():
+            # 変換するXMLファイルが存在しない
+            print("Source file not found:", srcFile)
+        else:
+            if path_src.suffix == '.zip':
+                # 一時ディレクトリを作成
+                with tempfile.TemporaryDirectory() as tmp_dir_name:
+                    path_tmp_dir = pathlib.Path(tmp_dir_name)
+
+                    # zipファイルを展開する
+                    with zipfile.ZipFile(srcFile) as zf:
+                        # ファイル名のリストを取り出す
+                        zf_names = zf.namelist()
+                         # 一時ディレクトリ内に展開
+                        zf.extractall(path_tmp_dir)
+
+                    for src_name in zf_names:
+                        srcFile = path_tmp_dir / src_name
+                        package.xml2geojson.SaveGeoJson(srcFile, dstDir, exclude_flag)
+
+            elif path_src.suffix != '.xml':
+                # 拡張子が.xmlではない
+                print('File extension is not .xml:', srcFile)
+                raise()
+            else:
+                package.xml2geojson.SaveGeoJson(srcFile, dstDir, exclude_flag)
+
     except Exception:
         print('Error Source File:', srcFile)
         traceback.print_exc()
